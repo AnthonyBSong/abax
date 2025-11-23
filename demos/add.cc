af881c09cd44d0ae402acbca59a84a176ca5c6f6
AnthonyBSong <anthony.b.song@gmail.com>
2025-11-22 19:54:51 -0500

    new changes to add.cc

--- demos/add.cc
+++ demos/add.cc
@@ -1,17 +1,19 @@
+
+
 template <int Width, bool Signed = true>
 using ac_int = XlsInt<Width, Signed>;
 
 class TestBlock {
 private:
   // Signed adds
-  ac_int<32, true> add(ac_int<32, true> a, ac_int<32, true> b) {
+  int add(int a, int b) {
     return a + b;
   }
 
 public:
   // Top method
   #pragma hls_top
-  ac_int<32, true> Run(ac_int<32, true> a, ac_int<32, true> b, int use) {
+  int Run(int a, int b, int use) {
       return add(a, b);
   }
 };
